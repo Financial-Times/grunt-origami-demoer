51ecbea22e0f1430507f24ca3c677744db5c80ee
Dan Searle <dan.searle@ft.com>
2014-02-26 17:21:36 +0000

    Custom Modernizr is now built from browserFeatures declared in module (and dependencies') origami.json files.

--- tasks/origami-demo-modernizr.js
+++ tasks/origami-demo-modernizr.js
@@ -2,9 +2,85 @@
 
 module.exports = function(grunt) {
 
+    var fs = require('fs');
+
+    function getBowerComponents() {
+        var bowerFolder = './bower_components/',
+            bowerFolderContents = fs.readdirSync(bowerFolder),
+            bowerComponentFolders = [];
+        bowerFolderContents.forEach(function(item){
+            if (fs.statSync(bowerFolder + item).isDirectory()) {
+                bowerComponentFolders.push(bowerFolder + item);
+            }
+        });
+        return bowerComponentFolders;
+    }
+
+    function getOrigamiJson(path) {
+        var origamiJson = false;
+        if (fs.existsSync(path)) {
+            origamiJson = JSON.parse(fs.readFileSync(path, { encoding: 'utf-8' }));
+        }
+        return origamiJson;
+    }
+
+    function getOrigamiJsons() {
+        var bowerModulePaths = getBowerComponents(),
+            origamiJsons = [];
+        bowerModulePaths.push('.'); // also include module's own bower.json
+        bowerModulePaths.forEach(function(path) {
+            var origamiJson = getOrigamiJson(path + '/origami.json');
+            if (origamiJson) {
+                origamiJsons.push(origamiJson);
+            }
+        });
+        return origamiJsons;
+    }
+
+    function addIfNew(a, v) {
+        if (a.indexOf(v) === -1) {
+            a.push(v);
+        }
+        return a;
+    }
+
+    function getOrigamiModuleFeatures() {
+        var origamiJsons = getOrigamiJsons(),
+            all = [],
+            required = [],
+            optional = [];
+        origamiJsons.forEach(function(origamiJson) {
+            if (origamiJson.browserFeatures) {
+                if (origamiJson.browserFeatures.required) {
+                    origamiJson.browserFeatures.required.forEach(function(feature){
+                        all = addIfNew(all, feature);
+                        required = addIfNew(required, feature);
+                    });
+                }
+                if (origamiJson.browserFeatures.optional) {
+                    origamiJson.browserFeatures.optional.forEach(function(feature){
+                        all = addIfNew(all, feature);
+                        optional = addIfNew(optional, feature);
+                    });
+                }
+            }
+        });
+        return {
+            all: all,
+            required: required,
+            optional: optional
+        }
+    }
+
     grunt.task.registerTask("origami-demo-modernizr", "", function() {
 
         grunt.loadTasks("./node_modules/grunt-origami-demoer/node_modules/grunt-modernizr/tasks");
+
+        var browserFeatures = getOrigamiModuleFeatures();
+
+        grunt.log.writeln("Reading module and dependencies' origami.json files...");
+        grunt.log.writeln("* Required browserFeatures: " + JSON.stringify(browserFeatures.required));
+        grunt.log.writeln("* Optional browserFeatures: " + JSON.stringify(browserFeatures.optional));
 
         grunt.config.set('modernizr.origami-demo', {
             "devFile" : "./node_modules/grunt-origami-demoer/src/lib/modernizr-latest.js",
@@ -17,9 +93,8 @@ module.exports = function(grunt) {
                 "mq" : false,
                 "cssclasses" : true
             },
-            "files": {
-                "src": ["demos/*.css", "demos/*.js"]
-            }
+            "parseFiles": false,
+            "tests": browserFeatures.all
         });
 
         grunt.task.run('modernizr');
